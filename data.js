8f4aee0d66a16a10639f101b2caf78d98bd54d7f
Nadya <nadus@ngs.ru>
2022-12-24 20:36:18 +0700

    сохранили data.js

---
+++ data.js
@@ -0,0 +1,32 @@
+const offer_content = `{
+    "productOne":{
+        "productName": "dressOne",
+        "prouctInform":"forMan",
+        "productPrice": 52
+    } 
+    "productTwo":{
+        "productName":"dressTwo",
+        "prouctInform":"forWomen",
+        "productPrice":52
+    }
+    "productThree":{
+        "productName":"dressThree",
+        "prouctInform":"forMan",
+        "productPrice":52
+    }
+    "productFour":{
+        "productName":"dressFour",
+        "prouctInform":"forMan",
+        "productPrice":52
+    }
+    "productFive":{
+        "productName":"dressFive",
+        "prouctInform":"forWomen",
+        "productPrice":52
+    }
+    "productSix":{
+        "productName":"dressSix",
+        "prouctInform":"forWomen",
+        "productPrice":52
+    }
+}`
